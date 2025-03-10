ad7d98d5308de805e14a40e373d4c3fbb3551c21
Maria Agatha Joson <137110758+agatha1108@users.noreply.github.com>
2025-03-10 19:52:26 +0800

    Update settings.py

--- form/settings.py
+++ form/settings.py
@@ -94,7 +94,8 @@ WSGI_APPLICATION = 'form.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
+
     }
 }
 
